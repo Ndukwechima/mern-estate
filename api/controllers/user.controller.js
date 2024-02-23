22a10ac2086bdb8bce5b4f6737ed99561aa3283d
Ndukwe Chima <chimandukwe.r@gmail.com>
2024-02-23 13:14:47 +0100

    first commit

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,6 @@
+export const test = (req, res) => {
+  res.json({
+    message: "Api rout is working",
+  });
+};
+// export default test;
---
+++ api/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    // console.log(err);
+  });
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
